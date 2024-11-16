b8348147a0f59172beab5c2a818ab0e112a73e76
Nguyễn Kiều Anh Quân <168531710+CharleSs0933@users.noreply.github.com>
2024-11-16 18:38:02 +0700

    Get User Task API

--- server/dist/src/controllers/taskController.js
+++ server/dist/src/controllers/taskController.js
@@ -9,7 +9,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.createTask = exports.updateTaskStatus = exports.getTasks = void 0;
+exports.getUserTasks = exports.createTask = exports.updateTaskStatus = exports.getTasks = void 0;
 const client_1 = require("@prisma/client");
 const prisma = new client_1.PrismaClient();
 const getTasks = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
@@ -75,3 +75,27 @@ const createTask = (req, res) => __awaiter(void 0, void 0, void 0, function* () 
     }
 });
 exports.createTask = createTask;
+const getUserTasks = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { userId } = req.params;
+    try {
+        const tasks = yield prisma.task.findMany({
+            where: {
+                OR: [
+                    { authorUserId: Number(userId) },
+                    { assignedUserId: Number(userId) },
+                ],
+            },
+            include: {
+                author: true,
+                assignee: true,
+            },
+        });
+        res.json(tasks);
+    }
+    catch (error) {
+        res
+            .status(500)
+            .json({ message: `Error retrieving user's task ${error.message}` });
+    }
+});
+exports.getUserTasks = getUserTasks;
